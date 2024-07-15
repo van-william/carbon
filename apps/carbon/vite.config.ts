51ab40e91aebfe8552e9d76c779c82f0bcfa8293
Brad Barbin <bradbarbin@protonmail.com>
2024-07-14 21:37:24 -0400

    build: upgrade packages

--- apps/carbon/vite.config.ts
+++ apps/carbon/vite.config.ts
@@ -12,55 +12,30 @@ installGlobals({
 
 export default defineConfig({
   ssr: {
-    noExternal: ["react-dropzone", "react-icons", "react-phone-number-input"],
+    noExternal: [
+      "novel",
+      "react-dropzone",
+      "react-icons",
+      "react-phone-number-input",
+    ],
   },
   server: {
     port: 3000,
   },
   plugins: [
     remix({
-      // appDirectory: "app",
-      // assetsBuildDirectory: "public/build",
-      // publicPath: "/build/",
-      // serverBuildPath: "build/index.js",
-      //browserNodeBuiltinsPolyfill: { modules: { events: true } },
-      // dev: {
-      //   port: 3601,
-      // },
       presets: [vercelPreset()],
       future: {
         unstable_singleFetch: true,
       },
       ignoredRouteFiles: ["**/.*"],
       serverModuleFormat: "esm",
-      //serverPlatform: "node",
-      //serverMinify: false,
       routes: async (defineRoutes) => {
         return flatRoutes("routes", defineRoutes, {
           // eslint-disable-next-line no-undef
           appDir: path.resolve(__dirname, "app"),
         });
       },
-      // serverDependenciesToBundle: [
-      //   "@carbon/database",
-      //   "@carbon/documents",
-      //   "@carbon/logger",
-      //   "@carbon/react",
-      //   "@carbon/remix-validated-form",
-      //   "@carbon/utils",
-      //   "nanoid",
-      //   "nanostores",
-      //   "@nanostores/react",
-      // ],
-      // watchPaths: async () => {
-      //   return [
-      //     "../../packages/database/src/**/*",
-      //     "../../packages/documents/src/**/*",
-      //     "../../packages/logger/src/**/*",
-      //     "../../packages/react/src/**/*",
-      //     "../../packages/utils/src/**/*",
-      //   ];
-      // },
     }),
     tsconfigPaths(),
   ],
