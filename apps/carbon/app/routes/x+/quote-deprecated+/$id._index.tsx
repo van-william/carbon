1686ae4ddf7f9c1089b9907a064f4090a39e52d3
Brad Barbin <bradbarbin@protonmail.com>
2024-07-27 12:16:02 -0400

    refactor: remove deprecated quote files

--- apps/carbon/app/routes/x+/quote-deprecated+/$id._index.tsx
+++
@@ -1,9 +0,0 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { path } from "~/utils/path";
-
-export async function loader({ params }: LoaderFunctionArgs) {
-  const { id } = params;
-  if (!id) throw new Error("Could not find id");
-  throw redirect(path.to.quoteDetails(id));
-}
