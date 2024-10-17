97ed7779e7a478edab7fa17fd87c60824b2df201
Brad Barbin <bradbarbin@protonmail.com>
2024-10-17 14:16:24 -0400

    refactor: use window.location.origin instead of envirnonment variable

--- apps/carbon/app/routes/_public+/confirm-invite.tsx
+++ apps/carbon/app/routes/_public+/confirm-invite.tsx
@@ -1,4 +1,4 @@
-import { SUPABASE_API_URL, VERCEL_ENV } from "@carbon/auth";
+import { SUPABASE_API_URL } from "@carbon/auth";
 import { Button as _Button, Heading as _Heading, VStack } from "@carbon/react";
 import { useNavigate, useSearchParams } from "@remix-run/react";
 import { AnimatePresence, motion } from "framer-motion";
@@ -22,11 +22,7 @@ export default function ConfirmInvite() {
   }
 
   const getConfirmationURL = (token: string) => {
-    const appUrl =
-      VERCEL_ENV === "production"
-        ? `https://app.carbonos.dev`
-        : "http://localhost:3000";
-    return `${SUPABASE_API_URL}/auth/v1/verify?token=${token}&type=invite&redirect_to=${appUrl}/callback`;
+    return `${SUPABASE_API_URL}/auth/v1/verify?token=${token}&type=invite&redirect_to=${window.location.origin}/callback`;
   };
 
   console.log(getConfirmationURL("xxxx"));
