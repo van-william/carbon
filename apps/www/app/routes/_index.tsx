2dc79a67189c6363e029be19d521fb806f1f37c7
Brad Barbin <bradbarbin@protonmail.com>
2024-10-31 09:20:26 -0400

    feat: add website

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -1,35 +1,23 @@
-import { Button, Heading } from "@carbon/react";
-import { BsHexagonFill } from "react-icons/bs";
-import { FaPlay } from "react-icons/fa6";
 import { LuMoveUpRight } from "react-icons/lu";
 
 export default function Route() {
   return (
     <>
       <header className="flex select-none items-center bg-background pl-5 pr-4 border-b h-[52px] border-transparent">
-        <div className="flex items-center gap-2 z-logo text-foreground cursor-pointer">
-          <a
-            href="/"
-            className="text-xl font-bold tracking-tighter text-foreground flex items-center gap-2"
-          >
-            <BsHexagonFill />
-          </a>
-        </div>
+        <div className="flex items-center gap-2 z-logo text-foreground cursor-pointer"></div>
       </header>
 
       <div className="relative flex h-full w-full items-start justify-center overflow-hidden bg-background">
         <a
           id="announcement"
-          href="https://google.com"
+          href="https://app.carbonos.dev"
           className="fixed top-10 left-1/2 -translate-x-1/2 group items-center border border-input rounded-full px-4 py-1.5 flex mb-4.5 text-xs hover:bg-alpha-gray-2 dark:hover:bg-gray-900 transition-theme"
           target="_blank"
           rel="noreferrer"
         >
-          <FaPlay className="mr-2" />
           CarbonOS
-          <span className="hidden md:inline-flex">
-            : The operating system for manufacturing
-          </span>
+          <span className="hidden md:inline-flex">: Now in Private Beta</span>
+          <LuMoveUpRight className="ml-2" />
           <span className="absolute -top-px left-[2.5rem] h-px w-[calc(100%-5rem)] bg-gradient-to-r from-emerald-600/0 via-emerald-600/80 to-emerald-600/0 opacity-0 group-hover:opacity-100 transition-opacity transition-theme"></span>
           <span className="absolute -top-px left-1/2 -translate-x-1/2 h-px w-[60px] bg-gradient-to-r from-emerald-300/0 via-emerald-300/50 to-emerald-300/0 mix-blend-overlay opacity-0 group-hover:opacity-100 transition-opacity transition-theme"></span>
         </a>
@@ -42,20 +30,28 @@ export default function Route() {
           <div className="light-ray ray-five"></div>
         </div>
 
-        <div id="intro" className="flex flex-col pt-[32vh] mx-auto w-container">
-          <Heading size="display" className="text-center">
-            Let's build something great
-          </Heading>
-          <p className="mb-6 text-center text-muted-foreground">
+        <div
+          id="intro"
+          className="flex flex-col pt-[28dvh] mx-auto w-container text-center"
+        >
+          <img
+            src="https://app.carbonos.dev/carbon-logo-light.png"
+            alt="CarbonOS"
+            className="w-64 mx-auto"
+          />
+          {/* <h1 className="text-[3rem] sm:text-[6rem] md:text-[10rem] lg:text-[14rem] font-bold tracking-tighter lg:tracking-[-0.75rem] text-foreground">
+            CarbonOS
+          </h1> */}
+          {/* <p className="mb-6 text-center text-muted-foreground">
             Realtime, collaborative, and powerful manufacturing software.
-          </p>
-          <Button
+          </p> */}
+          {/* <Button
             className="rounded-full w-fit mx-auto"
             size="lg"
             rightIcon={<LuMoveUpRight />}
           >
             Get Started
-          </Button>
+          </Button> */}
         </div>
       </div>
     </>
