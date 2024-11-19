44b7389a7f95f7c5f5ea9054abf3b1e6b7f87cb0
Brad Barbin <bradbarbin@protonmail.com>
2024-11-19 08:45:19 -0500

    feat: add material tables to realtime publication

---
+++ packages/database/supabase/migrations/20241119134231_realtime-saeki.sql
@@ -0,0 +1,5 @@
+
+ALTER PUBLICATION supabase_realtime ADD TABLE "material";
+ALTER PUBLICATION supabase_realtime ADD TABLE "materialForm";
+ALTER PUBLICATION supabase_realtime ADD TABLE "materialSubstance";
+ALTER PUBLICATION supabase_realtime ADD TABLE "customField";
