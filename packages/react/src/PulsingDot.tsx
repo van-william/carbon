b028770ba50e85711ba159a74a76947fbf6baed0
Brad Barbin <bradbarbin@protonmail.com>
2025-01-22 15:48:01 -0500

    feat: add pulsing dot component to shared library

---
+++ packages/react/src/PulsingDot.tsx
@@ -0,0 +1,21 @@
+import type { ComponentProps } from "react";
+import { cn } from "./utils/cn";
+
+export function PulsingDot({
+  inactive,
+  className,
+  ...props
+}: ComponentProps<"span"> & { inactive?: boolean }) {
+  if (inactive) {
+    return <span className="w-2 h-2 bg-muted rounded-full mt-2 bg-red-500" />;
+  }
+
+  return (
+    <span className={cn("relative flex h-2 w-2 mt-2", className)} {...props}>
+      <span
+        className={`absolute h-full w-full animate-ping rounded-full border border-emerald-500 opacity-100 duration-1000`}
+      />
+      <span className={`h-2 w-2 rounded-full bg-emerald-500`} />
+    </span>
+  );
+}
