60136386ea49bd1aa704c9895cedbcbf5358bec7
Brad Barbin <bradbarbin@protonmail.com>
2025-03-24 16:02:52 -0400

    feat: remove gravyfries@protonmail.com from isInternal FF

--- apps/erp/app/hooks/useFlags.tsx
+++ apps/erp/app/hooks/useFlags.tsx
@@ -2,11 +2,9 @@ import { useUser } from "./useUser";
 
 export function useFlags() {
   const user = useUser();
-  const isInternal = [
-    "carbon.us.org",
-    "carbonos.dev",
-    "gravyfries@protonmail.com",
-  ].some((email) => user.email.includes(email));
+  const isInternal = ["carbon.us.org", "carbonos.dev"].some((email) =>
+    user.email.includes(email)
+  );
 
   return {
     isInternal,
