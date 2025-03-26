3104b11262ddbee1bb5a6cfbff810a240aa73cb8
Brad Barbin <bradbarbin@protonmail.com>
2025-03-26 12:30:57 -0400

    feat: add sale orders api route

---
+++ apps/erp/app/routes/api+/sales.orders.ts
@@ -0,0 +1,12 @@
+import { requirePermissions } from "@carbon/auth/auth.server";
+import type { LoaderFunctionArgs } from "@vercel/remix";
+import { json } from "@vercel/remix";
+import { getSalesOrdersList } from "~/modules/sales";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { client, companyId } = await requirePermissions(request, {
+    view: "sales",
+  });
+
+  return json(await getSalesOrdersList(client, companyId));
+}
