f0a3779b1cba38330149ce1519df4e47c11397ed
Brad Barbin <bradbarbin@protonmail.com>
2025-05-20 15:10:01 -0400

    refactor: add logging to purchase order pdf

--- packages/documents/src/utils/shared.ts
+++ packages/documents/src/utils/shared.ts
@@ -3,6 +3,7 @@ export const getCurrencyFormatter = (
   locale: string,
   maximumFractionDigits?: number
 ) => {
+  console.log({ baseCurrencyCode, locale, maximumFractionDigits });
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: baseCurrencyCode,
