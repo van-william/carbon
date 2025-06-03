8e2be475b039021f0db16bc861e9e8dd98f60bec
Brad Barbin <bradbarbin@protonmail.com>
2025-06-02 21:37:49 -0400

    fix: remove migration from cherry-picked commit

--- packages/database/supabase/migrations/20250516124051_revisions.sql
+++
@@ -1,4 +0,0 @@
-
-ALTER TABLE "makeMethod" DROP CONSTRAINT "makeMethod_unique_itemId";
-ALTER TABLE "makeMethod" ADD CONSTRAINT "makeMethod_unique_itemId_revision" UNIQUE ("itemId", "revision");
-ALTER TABLE "makeMethod" ADD COLUMN "revision" TEXT NOT NULL DEFAULT '0';
